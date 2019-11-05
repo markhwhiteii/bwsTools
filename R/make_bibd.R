57a8ee26146c4fafa31584bc4d9e2eee67008658
markhwhiteii <markhwhiteii@gmail.com>
2019-11-05 16:58:20 -0500

    attempt at make_bibd working without loading bwstools

--- R/make_bibd.R
+++ R/make_bibd.R
@@ -12,7 +12,8 @@
 #'
 #' @export
 make_bibd <- function(design) {
-  tmp <- bibds[bibds$design == design, ]
+  tmp <- data("bibds")
+  tmp <- tmp[tmp$design == design, ]
   out <- crossdes::find.BIB(tmp$t, tmp$b, tmp$k)
   colnames(out) <- paste0("Option", 1:ncol(out))
   rownames(out) <- 1:nrow(out)
