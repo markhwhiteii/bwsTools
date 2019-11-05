85a0fc371a0398fc945375554e31f78d4ef1bb28
markhwhiteii <markhwhiteii@gmail.com>
2019-11-05 17:02:38 -0500

    change environment

--- R/make_bibd.R
+++ R/make_bibd.R
@@ -12,8 +12,8 @@
 #'
 #' @export
 make_bibd <- function(design) {
-  tmp <- data("bibds", package = "bwstools")
-  tmp <- tmp[tmp$design == design, ]
+  data("bibds", package = "bwstools", envir = environment())
+  tmp <- bibds[bibds$design == design, ]
   out <- crossdes::find.BIB(tmp$t, tmp$b, tmp$k)
   colnames(out) <- paste0("Option", 1:ncol(out))
   rownames(out) <- 1:nrow(out)
