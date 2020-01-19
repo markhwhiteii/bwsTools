fe85b22a7d36935dbfac169e52c2bc0c91817f0f
markhwhiteii <markhwhiteii@gmail.com>
2020-01-18 23:14:10 -0500

    remove tests relying on set.seed, as that appears to change between versions of R

--- tests/testthat/test_eloscoring.R
+++ tests/testthat/test_eloscoring.R
@@ -1,19 +1,6 @@
 context("Calculate individual Elo scores")
 library(bwsTools)
 
-test_that("computation is working as it should", {
-  set.seed(1839)
-  res <- eloscoring(indiv, "id", "block", "label", "value", iter = 1)
-  expect_equal(
-    round(res[res$id == 2 & res$label == "crime", "elo", drop = TRUE]),
-    1043
-  )
-  expect_equal(
-    round(res[res$id == 10 & res$label == "natsecurity", "elo", drop = TRUE]),
-    992
-  )
-})
-
 test_that("wide argument does what it is supposed to do", {
   res1 <- eloscoring(indiv, "id", "block", "label", "value", iter = 1)
   res2 <- eloscoring(indiv, "id", "block", "label", "value", 
--- tests/testthat/test_get_eloscores.R
+++ tests/testthat/test_get_eloscores.R
@@ -1,20 +1,6 @@
 context("Calculate Elo score for an individual")
 library(bwsTools)
 
-test_that("computation is working as it should", {
-  res <- get_eloresults(indiv[indiv$id == 2, ], "block", "label", "value")
-  set.seed(1839); res <- get_eloscores(res, iter = 5)
-  expect_equal(round(res[res$item == "abortion", "elo", drop = TRUE]), 861)
-  expect_equal(round(res[res$item == "education", "elo", drop = TRUE]), 999)
-  expect_equal(round(res[res$item == "taxes", "elo", drop = TRUE]), 1038)
-  
-  res <- get_eloresults(indiv[indiv$id == 8, ], "block", "label", "value")
-  set.seed(1839); res <- get_eloscores(res, iter = 1)
-  expect_equal(round(res[res$item == "abortion", "elo", drop = TRUE]), 927)
-  expect_equal(round(res[res$item == "education", "elo", drop = TRUE]), 1000)
-  expect_equal(round(res[res$item == "taxes", "elo", drop = TRUE]), 961)
-})
-
 test_that("K is working as it should", {
   res <- get_eloresults(indiv[indiv$id == 2, ], "block", "label", "value")
   set.seed(1839); res1 <- get_eloscores(res, K = 1, iter = 1)
